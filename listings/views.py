7165b526199900b7f7c694f429f764cb7f1d436d
Sage Chau <chausage@yahoo.com>
2024-12-24 01:34:45 +0800

    Update views.py

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Listing
+from listings.models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 def index(request):
